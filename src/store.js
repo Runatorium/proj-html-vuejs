0f469122cc930cafdb0455bd45c6c53a6ac75de6
Runatorium <stefano.alesci1@gmail.com>
2023-04-11 16:53:07 +0200

    Data structure Completed

--- src/store.js
+++ src/store.js
@@ -3,6 +3,17 @@ import bootstrapBundle from 'bootstrap/dist/js/bootstrap.bundle'
 
 export const store = reactive(
     {
+        menuvoices: [
+            'Home',
+            'Shop',
+            'About',
+            'Gallery',
+            'Locations',
+            'Journal',
+            'Contact',
+            'MyAccount'
+        ],
+
         products: [
             {
                 name: 'Choco chip Cookies',
@@ -32,6 +43,65 @@ export const store = reactive(
                 minPrice: 18.00,
                 maxPrice: 52.00,
             }
-        ]
+        ],
+        
+        events: [
+            {
+                name: 'Wedding & Parties',
+                imgurl: 'asd',
+                type: 'Private Dining Hall',
+            },
+            {
+                name: 'Internation Bakery',
+                imgurl: 'asd',
+                type: 'Corporate Events', 
+            },
+        ],
+
+        process: [
+            {
+                name: 'Tell us what you have in mind',
+                number: '01', 
+                subtitle: 'Manual Ingredient Selection',
+                imgurl: 'asd',
+                text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Provident ea nihil repellendus consequatur libero, distinctio aut corporis laborum, nisi numquam ratione est illo quis officia.'
+            },
+            {
+                name: 'We Start Baking',
+                number: '02', 
+                subtitle: 'Sweet & Delicious',
+                imgurl: 'asd',
+                text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ea nihil repellendus consequatur libero, distinctio aut corporis laborum, nisi numquam ratione est illo quis officia.'
+            },
+            {
+                name: 'Delivered to your home',
+                number: '03', 
+                subtitle: 'Baked to order & gift wrapped',
+                imgurl: 'asd',
+                text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ea nihil repellendus consequatur libero, distinctio aut corporis laborum, nisi numquam ratione est illo quis officia.'
+            },
+        ],
+
+        locations: [
+            {
+                city: 'New York',
+                imgurl: 'asd',
+                Pnumber: '1.800.458.556',
+                openinghour: '9:00 AM',
+                closinghour: '6:00 PM',
+                mapslocation: '#'
+            },
+
+            {
+                city: 'London',
+                imgurl: 'asd',
+                Pnumber: '1.800.458.556',
+                openinghour: '9:00 AM',
+                closinghour: '6:00 PM',
+                mapslocation: '#'
+            },
+        ],
+
+
     }
 );
