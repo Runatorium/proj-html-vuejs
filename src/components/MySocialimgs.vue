7837e02fe1bb4e7484e12f86ce7f815a7f593f87
Runatorium <stefano.alesci1@gmail.com>
2023-04-13 15:45:41 +0200

    Social Media Section Completed

--- src/components/MySocialimgs.vue
+++ src/components/MySocialimgs.vue
@@ -17,6 +17,10 @@ export default{
 <template>
     <div class="SocialMediaBox d-flex">
         <div class="socialmediainnerbox d-flex" >
+            <button id="followbtn">
+                
+                <p> <i class="fa-brands fa-instagram"></i> Follow Us</p>
+            </button>
             <div class="imgs" :id="imgs.mbr" :style="{backgroundImage: `url(${imgs.url})`}" v-for="imgs in store.socialmediaimgs">
                 <p id="greytitle" v-text="imgs.subtitle"></p>
                 <h2 class="title" v-text="imgs.title"></h2>
@@ -48,4 +52,32 @@ export default{
     height: 356px;
     background-color: var(--Pearl-Bush);
 }
+#greytitle{
+    padding: 20px 0px;
+    font-size: 16px;
+    color: var(--Manatee);
+    text-align: center;
+    margin-top: 80px;
+}
+.title{
+    text-align: center;
+    color: var(--Daisy-Bush);
+    font-size: 36px;
+    font-weight: bolder;
+}
+    #followbtn{
+    position: absolute;
+    margin-top: 15px;
+    border-style: none;
+    border-radius: 7px;
+    background-color: var(--Daisy-Bush);
+    margin-left: 37.5%;
+    margin-top: 220px;
+    }
+    #followbtn p{
+    padding: 10px 20px;
+    font-size: 12px;
+    color: var(--white);
+    font-weight: 500;
+    }
 </style>
