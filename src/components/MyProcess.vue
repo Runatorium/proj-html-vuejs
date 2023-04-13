bd8d3b78b0a359c735823aafb20207908872738d
Runatorium <stefano.alesci1@gmail.com>
2023-04-13 11:49:17 +0200

    Process Box completed

--- src/components/MyProcess.vue
+++ src/components/MyProcess.vue
@@ -20,6 +20,7 @@ export default{
     <div class="internalcontainer d-flex">
         <div :style="{backgroundImage: `url(${process.imgurl})`}" v-for="process in store.process" :id="process.id" class="processbox"> 
             <h1 class="title" v-text="process.name"></h1>
+            <h2 v-text="process.number"></h2>
             <p class="greytitle" v-text="process.subtitle"></p>
             <h4 class="text" v-text="process.text"></h4>
         </div>
@@ -76,10 +77,15 @@ export default{
         font-weight: 100;
         width: 50%;
     }
+    h2{
+        color: var(--white);
+        font-size: 52px;
+        font-weight: 100;
+    }
       /*Text chars*/
 
 
-    /*specifics*/
+    /*specifics 1*/
     #nmb1 .title{
         text-align: left;
         color: white;
@@ -97,9 +103,79 @@ export default{
         right: 0;
         width: 30%;
     }
+    #nmb1 h2{
+        position: absolute;
+        right: 0;
+        margin-right: 200px;
+        margin-top: -60px;
+    }
 
      /*specifics*/
 
+     /*specifics 2*/
+     #nmb2 .title{
+        text-align: left;
+        color: white;
+        padding: 15px;
+    }
+    #nmb2 .text{
+        margin-top: 35%;
+        width: 50%;
+        margin-left: 20px;
+    }
+    #nmb2 .greytitle{
+        position: absolute;
+        margin-top: 200px;
+        margin-left: 20px;
+        width: 30%;
+    }
+    #nmb2 h2{
+        position: absolute;
+        margin-left: 35%;
+        margin-top: -60px;
+    }
+      /*specifics*/
 
+      /*specifics 3*/
+      #nmb3 .title{
+        margin-top: 20%;
+        width: 70%;
+        margin-left: 15%;
+        text-align: center;
+        color: var(--Daisy-Bush);
+    }
 
+    #nmb3 .text{
+        width: 50%;
+        margin-left: 20px;
+    }
+
+    #nmb3 .greytitle{
+        position: relative;
+        margin-left: 20px;
+        width: 30%;
+    }
+   
+      /*specifics */
+      #nmb4 h2{
+        position: absolute;
+        right: 0;
+        margin-right: 200px;
+        margin-top: -40px;
+    }
+    #nmb4 .title{
+        text-align: left;
+        color: white;
+        padding: 15px;
+    }
+    #nmb4 .text{
+        margin-top: 20%;
+        width: 30%;
+        margin-left: 10px;
+    }
+    #nmb4 .greytitle{
+        position: absolute;
+        margin-top: 250px;
+        margin-left: 10px;
+    }
 </style>
